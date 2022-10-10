95ca1839bf768286d0088c4581100de2681df6aa
Lofi9 <starrymere@gmail.com>
2022-10-10 14:57:15 -0500

    Join message works

--- Ribbit/infrastructure/welcomeEmbed.py
+++ Ribbit/infrastructure/welcomeEmbed.py
@@ -1,4 +1,11 @@
-import hikari
+import hikari.guilds
+import hikari.messages
+import hikari.events
+from Ribbit import mainBot
 
 # https://www.hikari-py.dev/hikari/embeds.html
 
+@mainBot.bot.listen()
+async def on_event(event: hikari.MemberCreateEvent) -> None:
+    # await something something message send event
+    # RTFM !!!!!!!
