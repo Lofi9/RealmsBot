df0b24c4e0abd4f1fc17d2ab8d360f0ad013c5a7
Lofi9 <starrymere@gmail.com>
2022-10-11 16:49:02 -0500

    Ribbit C# Dev

---
+++ Ribbit/RibbitBot.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus;
+
+namespace mainBot
+{
+    public class RealmsBot
+    {
+        public static void Main(string[] args)
+        {
+            var bot = new DiscordShardedClient(new DiscordConfiguration());
+
+            
+            bot.StartAsync();
+        }
+    }
+}
