0c93c838452ea71b12a16a7bec395575214f7e9a
Lofi9 <starymere@gmail.com>
2022-10-28 12:36:32 -0500

    first commit

---
+++ ribbit_cargo/target/debug/deps/ribbit_cargo-f6ce667c2e514bf0.d
@@ -0,0 +1,5 @@
+/home/ino/RealmsBot/Ribbit/ribbit_cargo/target/debug/deps/ribbit_cargo-f6ce667c2e514bf0: src/main.rs
+
+/home/ino/RealmsBot/Ribbit/ribbit_cargo/target/debug/deps/ribbit_cargo-f6ce667c2e514bf0.d: src/main.rs
+
+src/main.rs:
---
+++ ribbit_cargo/target/debug/ribbit_cargo.d
@@ -0,0 +1,1 @@
+/home/ino/RealmsBot/ribbit_cargo/target/debug/ribbit_cargo: /home/ino/RealmsBot/ribbit_cargo/src/main.rs
