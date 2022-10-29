51aad9c4b0e677c7a1466c9156c9b3d0cf0cba79
Lofi9 <starrymere@gmail.com>
2022-10-28 20:26:27 -0500

    first run

--- Ribbit/main.py
+++ Ribbit/main.py
@@ -1,16 +1,8 @@
-import discord
-import os # default module
+import hikari
+import os
 from dotenv import load_dotenv
 
-load_dotenv() # load all the variables from the env file
-bot = discord.Bot()
+load_dotenv()
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user} is ready and online!")
-
-@bot.slash_command(name = "hello", description = "Say hello to the bot")
-async def hello(ctx):
-    await ctx.respond("Hey!")
-
-bot.run(os.getenv('TOKEN')) # run the bot with the token
+bot = hikari.GatewayBot(os.getenv('TOKEN'))
+bot.run()
