29ab8d9bf5a6c55dd8a5a95b869f007c837dff1b
Lofi9 <starrymere@gmail.com>
2022-10-28 19:54:39 -0500

    we're changing to python because they want to

--- ribbit_cargo/target/debug/deps/ribbit_cargo-f6ce667c2e514bf0.d
+++
@@ -1,5 +0,0 @@
-/home/ino/RealmsBot/Ribbit/ribbit_cargo/target/debug/deps/ribbit_cargo-f6ce667c2e514bf0: src/main.rs
-
-/home/ino/RealmsBot/Ribbit/ribbit_cargo/target/debug/deps/ribbit_cargo-f6ce667c2e514bf0.d: src/main.rs
-
-src/main.rs:
--- ribbit_cargo/target/debug/ribbit_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/ino/RealmsBot/ribbit_cargo/target/debug/ribbit_cargo: /home/ino/RealmsBot/ribbit_cargo/src/main.rs
